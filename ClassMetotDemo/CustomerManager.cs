4ae7b6cce4263870e31c4936fdd0481e6088c925
pdesgn <pdesgn@DESKTOP-LC4MO2N>
2021-02-20 19:08:09 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+           {
+            Console.WriteLine(customer.Name + "Added to Database");
+           }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + "Deleted from Database");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Tarlan ";
+            customer1.Surname = "Abdullayev";
+            customer1.Id = 672343;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Onur ";
+            customer2.Surname = "Saygin";
+            customer2.Id = 453424;
+
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Donald ";
+            customer3.Surname = "Trump";
+            customer3.Id = 66666;
+
+
+            Customer[] Customers = new Customer[] { customer1, customer2, customer3 };
+            foreach (var Customer in Customers)
+            {
+                Console.WriteLine("Customer Information");
+                Console.WriteLine("Name: " + Customer.Name);
+                Console.WriteLine("Surname: " + Customer.Surname);
+                Console.WriteLine("Id Number: " + Customer.Id);
+
+
+            }
+
+            CustomerManager CustomerManager = new CustomerManager();
+            CustomerManager.Add(customer1);
+            CustomerManager.Add(customer2);
+
+            CustomerManager CustomerManager1 = new CustomerManager();
+            CustomerManager1.Delete(customer1);
+            CustomerManager1.Delete(customer2);
+
+
+        }
+    }
+}
